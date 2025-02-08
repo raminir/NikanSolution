db48731456a3a3e9e2970106db1cac5c2dadcb62
Ramin Firouzabadi <Ramin@DESKTOP-3EQQFON>
2025-02-08 11:58:34 +0330

    Making some changes!

---
+++ UI/Helper/PersianDateHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+public static class PersianDateHelper
+{
+    public static string ToPersianDate(this DateTime date)
+    {
+        PersianCalendar pc = new PersianCalendar();
+        int year = pc.GetYear(date);
+        int month = pc.GetMonth(date);
+        int day = pc.GetDayOfMonth(date);
+        return $"{year}/{month.ToString("00")}/{day.ToString("00")}";
+    }
+}
